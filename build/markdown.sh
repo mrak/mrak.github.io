aaff42264cb3fa20bc9a3130314bb3463b2403c2
Eric Mrak <enmrak@gmail.com>
2025-03-14 22:03:17 -0700

    simplify markdown render logic

--- build/markdown.sh
+++ build/markdown.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 
-TMPCODE="$(mktemp)"
-IN_CODEBLOCK=
-CODEBLOCK_LANG=
+set -eu
+
+TMPCODE="$(mktemp)" IN_CODEBLOCK='' CODEBLOCK_LANG=''
 
 while IFS= read -r line; do
-  # code fence sighted
-  if expr "$line" : ' *```[^ ]* *' >/dev/null; then
-    # end code block, format seen code with chroma
-    if [ "$IN_CODEBLOCK" = 1 ]; then
+  if [ "$IN_CODEBLOCK" = 1 ]; then
+    if expr "$line" : ' *```[^ ]* *' >/dev/null; then
       IN_CODEBLOCK=
       chroma "$TMPCODE" --html --html-only -l "$CODEBLOCK_LANG"
       echo
-    # begin code block
     else
+      echo "$line" >> "$TMPCODE"
+    fi
+  else
+    if CODEBLOCK_LANG="$(expr "$line" : ' *```\([^ ]*\) *')"; then
       IN_CODEBLOCK=1
       : > "$TMPCODE"
-      CODEBLOCK_LANG="$(expr "$line" : ' *```\([^ ]*\) *')"
+    else
+      echo "$line"
     fi
-    continue
-  fi
-  # save code for later formatting
-  if [ "$IN_CODEBLOCK" = 1 ]; then
-    echo "$line" >> "$TMPCODE"
-  # save regular line
-  else
-    echo "$line"
   fi
 done | pandoc -f gfm -t html
 
